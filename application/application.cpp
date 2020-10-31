46f2fd82adf857c6a6e809453b872e3ccb44dd4f
Zuucker <kamigie504@student.polsl.pl>
2020-10-31 12:03:00 +0100

    finding the lines of words

--- application/application.cpp
+++ application/application.cpp
@@ -2,39 +2,39 @@
 #include <map>
 #include <fstream>
 #include <string>
+#include <vector>
 using namespace std;
-
-void record_line(map<string, int>& dictionary, string line)
-{
-	string delimiter = " ";
-	
-}
 
 int main()
 {
-	map<string, int>dictionary;
+	map<string,vector<int>>dictionary;
 	ifstream input("lorem.txt");
 	if (input.is_open())
 	{
 		string word;
+		int line_number = 1;
 		while (getline(input, word))
 		{
 			if (dictionary.find(word)==dictionary.end())
 			{
-				dictionary.insert({ word,1 });
+				dictionary.insert({ word, { line_number } });
 			}
 			else
 			{
-				dictionary[word]++;
+				dictionary[word].push_back(line_number);
 			}
+			++line_number;
 		}
 	}
 	
 	string tmp;
 	cout << "Chose a word: " << endl;
 	cin >> tmp;
-	cout << "This word " << tmp << " appearas "<<dictionary[tmp]<<" times." << endl;
-
+	cout << "This word " << tmp << endl;
+	for (int i = 0; i < dictionary[tmp].size(); i++)
+	{
+		cout << dictionary[tmp][i] << endl;
+	}
 	
 	return 0;
 }
